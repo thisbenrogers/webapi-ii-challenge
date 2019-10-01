9857461134414db919a543bdf1e1ffb9e13afef3
Ben <thisbenrogers@gmail.com>
2019-10-01 16:43:55 -0500

    :construction: Finds array of posts

---
+++ posts/posts-router.js
@@ -0,0 +1,49 @@
+const express = require('express');
+
+const Posts = require('../data/db');
+
+const router = express.Router();
+
+
+// POST /api/posts  ----  insert(post)
+// Creates a post using the information sent inside the request body.
+
+
+// POST /api/posts/:id/comments  ----  findPostComments(postId)
+// Creates a comment for the post with the specified id using
+// information sent inside of the request body.
+
+
+// GET /api/posts  ----  find()
+// Returns an array of all the post objects contained in the database.
+router.get('/', (req, res) => {
+    Posts.find(req.query)
+        .then(posts => {
+            res.status(200).json(posts)
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({
+                message: 'Error retrieving posts'
+            });
+        });
+});
+
+// GET /api/posts/:id  ----  findById(id)
+// Returns the post object with the specified id.
+
+
+// GET /api/posts/:id/comments  ----  findPostComments(id)
+// Returns an array of all the comment objects associated with the post with the specified id.
+
+
+// DELETE /api/posts/:id  ----  remove(id)
+// Removes the post with the specified id and returns the deleted post object. You may need to 
+// make additional calls to the database in order to satisfy this requirement.
+
+
+// PUT /api/posts/:id  ----  update(id, changes)
+// Updates the post with the specified id using data from the request body. 
+// Returns the modified document, NOT the original.
+
+module.exports = router;
