7e6367ca6653700293b4fdb82602a89a91692e20
Ben <thisbenrogers@gmail.com>
2019-10-01 17:36:59 -0500

    :construction: Adds GET for post by id

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -66,13 +66,32 @@ router.get('/', (req, res) => {
         .catch(error => {
             console.log(error);
             res.status(500).json({
-                message: 'Error retrieving posts'
+                error: "The posts information could not be retrieved."
             });
         });
 });
 
 // GET /api/posts/:id  ----  findById(id)
 // Returns the post object with the specified id.
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    Posts.findById(id)
+        .then(post => {
+            if (post) {
+                res.status(200).json(post)
+            } else {
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist."
+                })
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({
+                error: "The post information could not be retrieved."
+            })
+        })
+})
 
 
 // GET /api/posts/:id/comments  ----  findPostComments(id)
